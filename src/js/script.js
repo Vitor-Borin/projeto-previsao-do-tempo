27a0d0ff102218e40a95f04e24676f7c863041ea
Vitor Borin <vitor.borin06@gmail.com>
2024-11-07 19:17:15 -0300

    Adicionando autocomplete no buscar cidade

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,24 @@
-// coloque aqui a sua chave de api
 const chaveDaApi = "3d783118e6fa4ef882711235240711";
 
 const botaoDeBusca = document.querySelector(".btn-busca");
+const inputBusca = document.getElementById("input-busca");
+const listaSugestoes = document.querySelector(".lista-sugestoes");
 
 botaoDeBusca.addEventListener("click", async () => {
-  const cidade = document.getElementById("input-busca").value;
-
+  const cidade = inputBusca.value;
   if (!cidade) return;
 
   const dados = await buscarDadosDaCidade(cidade);
-
   if (dados) preencherDadosNaTela(dados, cidade);
 });
 
 async function buscarDadosDaCidade(cidade) {
   const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${chaveDaApi}&q=${cidade}&aqi=no&lang=pt`;
-
   const resposta = await fetch(apiUrl);
 
   if (resposta.status !== 200) return;
 
-  const dados = resposta.json();
-
+  const dados = await resposta.json();
   return dados;
 }
 
@@ -33,16 +30,47 @@ function preencherDadosNaTela(dados, cidade) {
   const iconeCondicao = dados.current.condition.icon;
 
   document.getElementById("cidade").textContent = cidade;
-
   document.getElementById("temperatura").textContent = `${temperatura} ºC`;
-
   document.getElementById("condicao").textContent = condicao;
-
   document.getElementById("umidade").textContent = `${umidade}%`;
-
-  document.getElementById(
-    "velocidade-do-vento"
-  ).textContent = `${velocidadeDoVento} km/h`;
-
+  document.getElementById("velocidade-do-vento").textContent = `${velocidadeDoVento} km/h`;
   document.getElementById("icone-condicao").setAttribute("src", iconeCondicao);
+}
+
+async function buscarSugestoesDeCidade(query) {
+  const apiUrl = `https://api.weatherapi.com/v1/search.json?key=${chaveDaApi}&q=${query}`;
+  const resposta = await fetch(apiUrl);
+  if (resposta.status !== 200) return [];
+
+  const sugestoes = await resposta.json();
+  return sugestoes;
+}
+
+inputBusca.addEventListener("input", async () => {
+  const query = inputBusca.value;
+  if (query.length < 3) {
+    limparSugestoes();
+    return;
+  }
+
+  const sugestoes = await buscarSugestoesDeCidade(query);
+  mostrarSugestoes(sugestoes);
+});
+
+function mostrarSugestoes(sugestoes) {
+  limparSugestoes();
+
+  sugestoes.forEach((sugestao) => {
+    const item = document.createElement("li");
+    item.textContent = sugestao.name;
+    item.addEventListener("click", () => {
+      inputBusca.value = sugestao.name;
+      limparSugestoes();
+    });
+    listaSugestoes.appendChild(item);
+  });
+}
+
+function limparSugestoes() {
+  listaSugestoes.innerHTML = "";
 }
